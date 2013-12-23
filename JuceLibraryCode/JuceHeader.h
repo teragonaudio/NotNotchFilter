8091c977316e3a5b7c8813b3166e12a2a115645f
Nik Reiman <nik.reiman@gmail.com>
2013-12-23 17:54:51 +0100

    Update project settings

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -33,8 +33,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "NotNotchFilter";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "2.0.0";
+    const int          versionNumber  = 0x20000;
 }
 
 #endif   // __APPHEADERFILE_RJPC09__
