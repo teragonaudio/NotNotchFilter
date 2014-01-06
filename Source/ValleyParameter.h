54adcb4b41e2a90dae06aef5e79e372e0d7f646e
Nik Reiman <nik.reiman@gmail.com>
2014-01-06 11:15:11 +0100

    Add copyright to source code

--- Source/ValleyParameter.h
+++ Source/ValleyParameter.h
@@ -1,12 +1,23 @@
 /*
-  ==============================================================================
-
-    ValleyParameter.h
-    Created: 1 Jan 2014 9:01:17pm
-    Author:  Nik Reiman
-
-  ==============================================================================
-*/
+ * Copyright (c) 2013 - Teragon Audio LLC
+ *
+ * Permission is granted to use this software under the terms of either:
+ * a) the GPL v2 (or any later version)
+ * b) the Affero GPL v3
+ *
+ * Details of these licenses can be found at: www.gnu.org/licenses
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * ------------------------------------------------------------------------------
+ *
+ * This software uses the JUCE library.
+ *
+ * To release a closed-source product which uses JUCE, commercial licenses are
+ * available: visit www.juce.com for more information.
+ */
 
 #ifndef VALLEYPARAMETER_H_INCLUDED
 #define VALLEYPARAMETER_H_INCLUDED
